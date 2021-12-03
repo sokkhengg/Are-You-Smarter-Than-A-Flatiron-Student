9ecd7e9e4dc77ab9d4687645626330979b6e31d6
smh1988-2 <seamhurley1988@gmail.com>
2021-12-03 14:55:00 -0800

    testing API calls

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[questions, setQuestions] = useState([]);
+  
+  useEffect(() => {
+    fetch("https://jservice.io/api/random?count=10")
+    .then(r => r.json())
+    .then(question => setQuestions({...questions, question}))
+    .then(console.log(questions))}
+  , [])
+  
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     {(console.log(questions))}
+   </div>
   );
 }
 
