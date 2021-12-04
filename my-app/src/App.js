2adef6caf0b625ebe1b9995e5838587ed2363dc1
Sokheng <jiangshuxing009@gmail.com>
2021-12-04 12:38:47 -0800

    add hello from header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,11 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import Test from "./Test.js";
 import 'semantic-ui-css/semantic.min.css'
+import Header from "./Header"
 
 function App() {
   const[questions, setQuestions] = useState([]);
   const LOCAL_API = "http://localhost:3000/questions"
-
-  // useEffect(() => {
-  //   fetch("https://jservice.io/api/random?count=100")
-  //   .then(r => r.json())
-  //   .then(question => setQuestions({...questions, question}))
-  // }
-  // , [])
 
   useEffect(() => {
     fetch(LOCAL_API)
@@ -20,28 +14,10 @@ function App() {
     .then(question => setQuestions(question))
   }, []
   )
-  
-  console.log("hi")
-  console.log("Hello from Sean")
-  console.log("hi2")
-
-  console.log("Hello from sok")
-  console.log("Hello from sok")
-  console.log("Hello from sok")
-  console.log("Hello from sok")
-  
-
-  console.log("Hello from Sean")
-  console.log("Hello from Sean")
-  console.log("Hello from Sean")
-  console.log("Hello from Sean")
-  console.log("Hello from Sean")
-
-
 
   return (
    <div>
-     
+     <Header {...questions}/>
      {questions.map(q => {
        return (
          <div>
---
+++ my-app/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            console.log("hello from header")
+        </div>
+    )
+}
+
+export default Header
