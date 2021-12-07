172be557c1254f1f831e7141ca92a8c81ee6c9c8
Sokheng <jiangshuxing009@gmail.com>
2021-12-07 14:56:42 -0800

    logo and header

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import {Link} from "react-router-dom" 
+import {Grid, GridColumn} from "semantic-ui-react"
+
 function Header() {
     return (
         <div className="header">
         <Link to="/">
-            <h1>
-            Welcome to Quiz App
-            </h1>
-            <hr className="headerDivider"/>
+            <Grid columns="equal">
+                <GridColumn></GridColumn>
+                <GridColumn textAlign="center">
+                    <h1>
+                    Welcome to Quiz App
+                    </h1>
+                    <hr className="headerDivider"/>
+                </GridColumn>
+                <GridColumn></GridColumn>
+            </Grid>
+           
 
         </Link>
         </div>
--- my-app/src/Logo.js
+++ my-app/src/Logo.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import quiz from './quiz.jpg'
+import {Grid, GridColumn} from "semantic-ui-react"
 
 function Logo() {
     return (
         <div>
-            <img className="photo" 
-            src={quiz} alt="cat logo" width="270" height="130" />
+            <Grid columns="equal">
+                <GridColumn></GridColumn>
+                <GridColumn textAlign="center">
+                <img className="photo" 
+                    src={quiz} alt="cat logo" width="270" height="130" />
+                </GridColumn>
+                <GridColumn></GridColumn>
+            </Grid>
+           
         </div>
     )
 }
