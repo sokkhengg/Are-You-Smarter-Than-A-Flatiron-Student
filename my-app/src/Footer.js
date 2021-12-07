f9792c9761df83318de63fb55a8c18d9e67b3b8c
smh1988-2 <seamhurley1988@gmail.com>
2021-12-06 21:17:38 -0800

    Uh oh

---
+++ my-app/src/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
+
+function Footer() {
+    return (
+        <div>
+            <Grid columns="equal">
+                <Grid.Column>
+                </Grid.Column>
+
+                <Grid.Column textAlign="center">
+                    <p>Made by Sean and Sok.</p>
+                </Grid.Column>
+
+                <Grid.Column>
+                </Grid.Column>
+            </Grid>
+            
+        </div>
+    )
+}
+
+export default Footer
---
+++ my-app/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            Quick questions to learn about general knowledge
+        </div>
+    )
+}
+
+export default Header
