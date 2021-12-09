731ede1aec31e3571febbd26a646fb25cdf2a840
smh1988-2 <seamhurley1988@gmail.com>
2021-12-08 23:44:28 -0800

    Added new nav, icons, sidebar

--- my-app/src/QuestionContainer.js
+++ my-app/src/QuestionContainer.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Grid, Card } from "semantic-ui-react";
+import { Grid, Card, Button } from "semantic-ui-react";
 import AnswerContainer from "./AnswerContainer";
 
 
 export default function QuestionContainer({question,
-   allQuestions, setQuestions, playerStat, setPlayerStat}) {
+   allQuestions, setQuestions, playerStat, setPlayerStat, gameOptions, visible, setVisible }) {
 
   return (
       <Grid columns="equal">
@@ -13,12 +13,22 @@ export default function QuestionContainer({question,
           <Card fluid>
             <Card.Content>
               <Card.Meta textAlign="center">
-              <span>${question[0].value}</span>
+              {playerStat.answered}/{gameOptions.length} - <span>${question[0].value}</span>
               </Card.Meta>
               <br />
               <Card.Header textAlign="center">{question[0].question}</Card.Header>
               <br />
               <AnswerContainer answer={question[0].answer} question={question[0].value} allQuestions={allQuestions} setQuestions={setQuestions} playerStat={playerStat} setPlayerStat={setPlayerStat} />
+                
+                <Grid columns="equal">
+                <Grid.Column></Grid.Column>
+                  <Grid.Column>
+              <Button checked={visible} onClick={(e, data) => setVisible(!visible)} color="yellow">
+                  Show performance</Button>
+                </Grid.Column>
+                <Grid.Column></Grid.Column>
+                </Grid>
+
             </Card.Content>
           </Card>
         </Grid.Column>
